83f7713a4e5b8f5b2462e864aecf2220fa2440c6
OlisaPaul <odirahchukwumma28@gmail.com>
2025-04-19 11:42:49 +0100

    feat: enhance Navbar component with sticky behavior and responsive styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 interface DropdownLink {
   title: string;
@@ -18,27 +18,62 @@ interface NavbarProps {
   navLinks: NavLink[];
 }
 
-const Navbar: React.FC<NavbarProps> = ({ brand = "Feel Nigeria", navLinks }) => {
+const Navbar: React.FC<NavbarProps> = ({
+  brand = "Feel Nigeria",
+  navLinks,
+}) => {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      setIsSticky(scrollTop > 40); // TopBar height is 40px
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <div className="container-fluid p-0 fixed-top">
+    <div className={`container-fluid p-0  ${isSticky ? "sticky-top" : ""}`}>
       <nav className="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
-        <Link to="/" className="navbar-brand p-0">
-          <h1 className="m-0"><i className="fa fa-map-marker-alt me-3"></i>{brand}</h1>
+        <Link
+          to="/"
+          className={`navbar-brand p-0 ${isSticky ? "text-white" : ""}`}
+        >
+          <h1 className="m-0">
+            <i className="fa fa-map-marker-alt me-3"></i>
+            {brand}
+          </h1>
         </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
+        >
           <span className="fa fa-bars"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarCollapse">
-          <div className="navbar-nav ms-auto py-0">
+          <div className={`navbar-nav ms-auto py-0`}>
             {navLinks.map((link) =>
               link.dropdown ? (
                 <div className="nav-item dropdown" key={link.title}>
-                  <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                  <a
+                    href="#"
+                    className={`nav-link dropdown-toggle ${
+                      isSticky ? "text-white" : ""
+                    }`}
+                    data-bs-toggle="dropdown"
+                  >
                     {link.title}
                   </a>
                   <div className="dropdown-menu m-0">
                     {link.dropdown.map((sublink) => (
-                      <Link to={sublink.href} className="dropdown-item" key={sublink.title}>
+                      <Link
+                        to={sublink.href}
+                        className="dropdown-item"
+                        key={sublink.title}
+                      >
                         {sublink.title}
                       </Link>
                     ))}
@@ -46,8 +81,10 @@ const Navbar: React.FC<NavbarProps> = ({ brand = "Feel Nigeria", navLinks }) => 
                 </div>
               ) : (
                 <Link
-                  to={link.href ?? '#'}
-                  className={`nav-item nav-link ${link.active ? 'active' : ''}`}
+                  to={link.href ?? "#"}
+                  className={`nav-item nav-link ${
+                    isSticky ? "text-white" : ""
+                  } ${link.active ? "active" : ""}`}
                   key={link.title}
                 >
                   {link.title}
@@ -55,7 +92,12 @@ const Navbar: React.FC<NavbarProps> = ({ brand = "Feel Nigeria", navLinks }) => 
               )
             )}
           </div>
-          <Link to="/book" className="btn btn-primary rounded-pill py-2 px-4 ms-lg-4">Book Now</Link>
+          <Link
+            to="/book"
+            className="btn btn-primary rounded-pill py-2 px-4 ms-lg-4"
+          >
+            Book Now
+          </Link>
         </div>
       </nav>
     </div>
