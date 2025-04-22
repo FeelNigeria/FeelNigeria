e628ec49967187f4e065ca365465aa376d399f36
OlisaPaul <odirahchukwumma28@gmail.com>
2025-04-22 10:03:31 +0100

    refactor: remove unused imports from DestinationOverlay and SearchIcon components

--- src/components/DestinationOverlay.tsx
+++ src/components/DestinationOverlay.tsx
@@ -1,7 +1,3 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDragon } from "@fortawesome/free-solid-svg-icons";
-
 interface Props {
   location: string;
   dataLightbox: string;
