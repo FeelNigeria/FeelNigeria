5535a2180522ba178af54c5a25b8ff9fb968b335
OlisaPaul <odirahchukwumma28@gmail.com>
2025-04-24 06:14:34 +0100

    refactor: simplify Navbar component by removing props and hardcoding links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -21,12 +21,7 @@ interface Props {
   onSelectNavLink: (title: string) => void;
 }
 
-const Navbar: React.FC<Props> = ({
-  navLinks,
-  onSelectNavLink,
-  selectedNavLink,
-  brand = "Feel Nigeria",
-}: Props) => {
+const Navbar: React.FC<Props> = () => {
   const [isSticky, setIsSticky] = useState(false);
 
   useEffect(() => {
@@ -40,85 +35,84 @@ const Navbar: React.FC<Props> = ({
   }, []);
 
   return (
-    <div className={`container-fluid p-0`}>
-      <nav
-        className={`navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-4 py-lg-1 ${
-          isSticky ? "bg-light" : ""
-        }`}
+    <nav
+      className={
+        "navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0" +
+        (isSticky ? " position-fixed" : "")
+      }
+    >
+      <a href="" className="navbar-brand p-0">
+        <h1 className="m-0">
+          <i className="fa fa-map-marker-alt me-3"></i>Feel Nigeria
+        </h1>
+        {/* <!-- <img src="img/logo.png" alt="Logo"> --> */}
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse"
       >
-        <Link
-          to="/"
-          className={`navbar-brand p-0 ${
-            isSticky ? "text-success" : "text-white"
-          }`}
-        >
-          <h1 className="m-0">
-            <i className="fa fa-map-marker-alt me-3"></i>
-            {brand}
-          </h1>
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-        >
-          <span className="fa fa-bars"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <div className={`navbar-nav ms-auto py-0`}>
-            {navLinks.map((link) =>
-              link.dropdown ? (
-                <div
-                  className={`nav-item dropdown ${
-                    isSticky ? "text-white" : "text-green"
-                  }`}
-                  key={link.title}
-                >
-                  <a
-                    href="#"
-                    className={`nav-link dropdown-toggle text-white`}
-                    data-bs-toggle="dropdown"
-                  >
-                    {link.title}
-                  </a>
-                  <div className="dropdown-menu m-0">
-                    {link.dropdown.map((sublink) => (
-                      <Link
-                        to={sublink.href}
-                        className="dropdown-item"
-                        key={sublink.title}
-                      >
-                        {sublink.title}
-                      </Link>
-                    ))}
-                  </div>
-                </div>
-              ) : (
-                <Link
-                  to={link.href ?? "#"}
-                  className={`nav-item nav-link ${
-                    isSticky ? "" : "text-white"
-                  } ${
-                    link.title == selectedNavLink ? "bg-success text-white" : ""
-                  }`}
-                  key={link.title}
-                  onClick={() => onSelectNavLink(link.title)}
-                >
-                  {link.title}
-                </Link>
-              )
-            )}
+        <span className="fa fa-bars"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <div className="navbar-nav ms-auto py-0">
+          <a href="index.html" className="nav-item nav-link active">
+            Home
+          </a>
+          <a href="about.html" className="nav-item nav-link">
+            About
+          </a>
+          <a href="services.html" className="nav-item nav-link">
+            Services
+          </a>
+          <a href="packages.html" className="nav-item nav-link">
+            Packages
+          </a>
+          <a href="blog.html" className="nav-item nav-link">
+            Blog
+          </a>
+          <div className="nav-item dropdown">
+            <a
+              href="#"
+              className="nav-link dropdown-toggle"
+              data-bs-toggle="dropdown"
+            >
+              Pages
+            </a>
+            <div className="dropdown-menu m-0">
+              <a href="destination.html" className="dropdown-item">
+                Destination
+              </a>
+              <a href="tour.html" className="dropdown-item">
+                Explore Tour
+              </a>
+              <a href="booking.html" className="dropdown-item">
+                Travel Booking
+              </a>
+              <a href="gallery.html" className="dropdown-item">
+                Our Gallery
+              </a>
+              <a href="guides.html" className="dropdown-item">
+                Travel Guides
+              </a>
+              <a href="testimonial.html" className="dropdown-item">
+                Testimonial
+              </a>
+              <a href="404.html" className="dropdown-item">
+                404 Page
+              </a>
+            </div>
           </div>
-          <Link
-            to={"/booking"}
-            className="btn btn-success rounded-pill py-2 px-4 ms-lg-4"
-          >
-            Book Now
-          </Link>
+          <a href="contact.html" className="nav-item nav-link">
+            Contact
+          </a>
         </div>
-      </nav>
-    </div>
+        <a href="" className="btn btn-success rounded-pill py-2 px-4 ms-lg-4">
+          Book Now
+        </a>
+      </div>
+    </nav>
   );
 };
 
