ddea9c7c57cf469c0117e7ddafaaff14d8f8026e
OlisaPaul <odirahchukwumma28@gmail.com>
2025-04-30 22:50:52 +0100

    feat: add CarouselFade component with destination images and descriptions
    - Implemented CarouselFade component to display popular destinations in a carousel format.
    - Integrated OwlCarousel for smooth transitions and autoplay functionality.
    - Created DestinationDescription component to show title and description for each destination.
    - Added links for booking in the DestinationDescription component.
    - Utilized getValidImageUrl service to fetch valid image URLs for destinations.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,44 +1,37 @@
 import AboutUs from "./AboutUs";
-import Blog from "./Blog";
 import Destination from "./Destination";
-import Explore from "./Explore";
-import Gallery from "./Gallery";
 import HeroCarousel from "./HeroCarousel";
-import Package from "./Package";
 import ServicesSection from "./Services";
-import Subscribe from "./Subscribe";
 import TourBooking from "./TourBooking";
 import TravelGuide from "./TravelGuide";
-import slideOneImage from "../assets/img/carousel-1.jpg";
-import slideTwoImage from "../assets/img/carousel-2.jpg";
-import slideThreeImage from "../assets/img/carousel-3.jpg";
+import CarouselFade from "./CarouselFade";
 
-const heroSlides = [
-  {
-    image: slideOneImage,
-    alt: "Slide 1",
-    subheading: "Feel Nigeria",
-    heading: "Let's tour Nigeria Together!",
-    text: "Lets' show you the beautiful terrain of Nigeria",
-    buttonText: "Discover Now",
-  },
-  {
-    image: slideTwoImage,
-    alt: "Slide 2",
-    subheading: "Feel Nigeria",
-    heading: "Find Your Perfect Travel Guide",
-    text: "We make you feel @ home while enjoying your holiday in Nigeria",
-    buttonText: "Discover Now",
-  },
-  {
-    image: slideThreeImage,
-    alt: "Slide 3",
-    subheading: "Feel Nigeria",
-    heading: "You Like To Go?",
-    text: "Let's wow you while showing you the beautiful landscapes of Nigeria",
-    buttonText: "Discover Now",
-  },
-];
+// const heroSlides = [
+//   {
+//     image: slideOneImage,
+//     alt: "Slide 1",
+//     subheading: "Feel Nigeria",
+//     heading: "Let's tour Nigeria Together!",
+//     text: "Lets' show you the beautiful terrain of Nigeria",
+//     buttonText: "Discover Now",
+//   },
+//   {
+//     image: slideTwoImage,
+//     alt: "Slide 2",
+//     subheading: "Feel Nigeria",
+//     heading: "Find Your Perfect Travel Guide",
+//     text: "We make you feel @ home while enjoying your holiday in Nigeria",
+//     buttonText: "Discover Now",
+//   },
+//   {
+//     image: slideThreeImage,
+//     alt: "Slide 3",
+//     subheading: "Feel Nigeria",
+//     heading: "You Like To Go?",
+//     text: "Let's wow you while showing you the beautiful landscapes of Nigeria",
+//     buttonText: "Discover Now",
+//   },
+// ];
 
 const Home = () => {
   return (
@@ -46,6 +39,7 @@ const Home = () => {
       <HeroCarousel />
       <AboutUs />
       <ServicesSection />
+      <CarouselFade />
       <Destination />
       {/* <Subscribe /> */}
       {/* <Explore /> */}
